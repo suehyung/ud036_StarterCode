97da0dae6f17cf07ae0baa0ac48950f5c30993d5
suehyung <33367526+suehyung@users.noreply.github.com>
2017-11-18 14:12:55 -0800

    Create class media.Movie to store movie info

---
+++ media.py
@@ -0,0 +1,13 @@
+import webbrowser
+
+# Creates a class, Movie, which takes variables for title, storyline, the URL for poster_image, and 
+# the URL for trailer_youtube. It also defines a method, show_trailer, which opens the URL for trailer_youtube
+class Movie():
+    def __init__(self, title, storyline, poster_image, trailer_youtube):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
