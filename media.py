8e2e0cfbc8e3834393e0be0b40de61db68a0dd5b
suehyung <33367526+suehyung@users.noreply.github.com>
2017-11-19 01:05:25 -0800

    Edit for style

--- media.py
+++ media.py
@@ -1,7 +1,11 @@
 import webbrowser
 
-# Creates a class, Movie, which takes variables for title, storyline, the URL for poster_image, and 
-# the URL for trailer_youtube. It also defines a method, show_trailer, which opens the URL for trailer_youtube
+"""Creates a class, Movie, which takes variables for title, storyline,
+the URL for poster_image, and the URL for trailer_youtube. It also
+defines a method, show_trailer, which opens the URL for trailer_youtube
+"""
+
+
 class Movie():
     def __init__(self, title, storyline, poster_image, trailer_youtube):
         self.title = title
--- movies.py
+++ movies.py
@@ -1,45 +1,48 @@
 import media
 import fresh_tomatoes
 
-# These are instances of class media.Movie, populated with strings for title, storyline, poster_image, and trailer_youtube
+# These are instances of class media.Movie, populated with strings for
+# title, storyline, poster_image, and trailer_youtube
+
 the_rock = media.Movie(
     "The Rock",
     "Navy SEALs and their crazy hijinks",
-    "https://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_%28movie%29.jpg", 
+    "https://upload.wikimedia.org/wikipedia/en/8/82/The_Rock_%28movie%29.jpg",  # NOQA
     "https://www.youtube.com/watch?v=jGVJx5mOtL8"
     )
 back_future = media.Movie(
     "Back To The Future",
     "Teenager and his Delorean",
-    "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg",  # NOQA
     "https://www.youtube.com/watch?v=qvsgGtivCgs"
     )
 bladerunner = media.Movie(
     "Blade Runner",
     "More human than human",
-    "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/5/53/Blade_Runner_poster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=eogpIG53Cis"
     )
 empire = media.Movie(
     "The Empire Strikes Back",
     "Who's your daddy?",
-    "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg",  # NOQA
     "https://www.youtube.com/watch?v=xESiohGGP7g"
     )
-dazed_confused = media.Movie(
+dazed = media.Movie(
     "Dazed And Confused",
     "Party time in the 70s",
-    "https://upload.wikimedia.org/wikipedia/en/a/af/Dazed_and_Confused_%281993%29_poster.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/a/af/Dazed_and_Confused_%281993%29_poster.jpg",  # NOQA
     "https://www.youtube.com/watch?v=Mvj4Ng6yinA"
     )
 groundhog = media.Movie(
     "Groundhog Day",
     "Stuck in Punxsutawney, PA",
-    "https://upload.wikimedia.org/wikipedia/en/b/b1/Groundhog_Day_%28movie_poster%29.jpg",
+    "https://upload.wikimedia.org/wikipedia/en/b/b1/Groundhog_Day_%28movie_poster%29.jpg",  # NOQA
     "https://www.youtube.com/watch?v=tSVeDx9fk60"
     )
 
 # Creates a list of movie objects
-movies = [the_rock, back_future, bladerunner, empire, dazed_confused, groundhog]
-# Calls the function that creates and opens in a new tab an HTML file using the list of movie objects above
+movies = [the_rock, back_future, bladerunner, empire, dazed, groundhog]
+# Calls the function that creates and opens in a new tab an HTML file using
+# the list of movie objects above
 fresh_tomatoes.open_movies_page(movies)
